cc0b899c66eb883489424685d4b946472b8cce40
F1rr1n <eaasemo@students.eaaa.dk>
2018-05-22 12:34:03 +0200

    Add pickups, ryddet op

--- javascript/script.js
+++ javascript/script.js
@@ -9,8 +9,9 @@ var bounds = new google.maps.LatLngBounds();
 
 
 
-            // Multiple Markers
+            // Multiple Markers title and coordinates lat&lng
             var markers = [
+              ['Nordjyske Motorvej 285, 9230 Aalborg',56.992715, 9.904865],
               ['Ringvej syd 104. 8260 Viby', 56.117239, 10.150283],
               ['Møllegade 103, 8660 Skanderborg', 56.039304, 9.917008],
               ['Birkedam 14, Harte, 6000 Kolding', 55.518266, 9.444145],
@@ -19,20 +20,8 @@ var bounds = new google.maps.LatLngBounds();
               ['Egevej 6, 6200 Aabenraa', 55.066151, 9.365841],
 ];
 
-
-            // Info Window Content
-            var infoWindowContent = [
-                ['<div class="info_content">' +
-                '<h3></h3>' +
-                '<p></p>' +        '</div>'],
-                ['<div class="info_content">' +
-                '<h3></h3>' +
-                '<p></p>' +
-                '</div>']
-            ];
-
             // Display multiple markers on a map
-            var infoWindow = new google.maps.InfoWindow(), marker, i;
+
 
             // Loop through our array of markers & place each one on the map
             for( i = 0; i < markers.length; i++ ) {
@@ -63,41 +52,3 @@ var bounds = new google.maps.LatLngBounds();
             });
 
         }
-
-
-
-// Kærgårdsvej, 8700 Horsens
-// Egevej 6, 6200 Aabenraa
-
-    // var aarhus = {lat: 56.117239, lng: 10.150283};
-    // var skanderborg = {lat: 56.039304, lng: 9.917008};
-    // var kolding = {lat: 55.518266, lng:9.444145 };
-    // var vejle = { lat:55.685547, lng:9.560671 };
-
-
-  //   var map = new google.maps.Map(document.getElementById('map'), {
-  //     zoom: 7,
-  //     center: aarhus
-  //   });
-  //
-  //   var markers = new google.maps.Marker({
-  //     position: skanderborg,
-  //     map: map,
-  //     title: 'Møllegade 103, 8660 Skanderborg'});
-  //
-  //   var marker = new google.maps.Marker({
-  //     position: aarhus,
-  //     map: map,
-  //     title: 'Ringvej syd 104. 8260 Viby'
-  //   });
-  //   var mark = new google.maps.Marker ({
-  //     position: kolding,
-  //     map:map,
-  //     title: 'Birkedam 14, Harte, 6000'
-  //   })
-  //   var marks = new google.maps.Marker ({
-  //     position: vejle,
-  //     map: map,
-  //     title: 'Fredericiavej 151, 7100',
-  //   })
-  // }
